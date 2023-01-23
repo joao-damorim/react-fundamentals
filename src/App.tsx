ef397afd67c4e919f1239321930d60e3480e7276
João D'Amorim <joao.damorim@gmail.com>
2023-01-23 10:46:37 -0300

    Changing jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { SourceMap } from 'module';
 
 function App() {
   const name = 'João'
+  const newName = name.toUpperCase()
+
+  const url = 'https://via.placeholder.com/150'
+
+  function sum(a: number, b: number) {
+    return a + b
+  }
+
   return (
     <div className="App">
       <h1>Alterando o JSX</h1>
-      <h3>Meu nome é: { name }</h3>
+      <h3>Meu nome é { newName }</h3>
+      <h3>A soma é {sum(1,3)}</h3>
+      <img src={url} alt="150x150"/>
     </div>
   );
 }
