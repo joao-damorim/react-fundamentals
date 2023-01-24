86b8b25cb1c20d080d4aca1dd6cf163f0c7efb81
João D'Amorim <joao.damorim@gmail.com>
2023-01-24 15:28:22 -0300

    React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,22 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { SourceMap } from 'module';
-import HelloWorld from './components/HelloWorld';
-import SayMyName from './components/SayMyName';
-import Person from './components/Person';
-import List from './components/List';
-import Event from './components/Event';
-import Form from './components/Form';
-import Conditional from './components/Conditional';
-import AnotherList from './components/AnotherList';
-import YourName from './components/YourName';
-import Salutation from './components/Salutation';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
+import Company from './pages/Company';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
 
 function App() {
 
-  const name = 'Maria'
-
-  const myItems = ['React', 'Vue', 'Angular']
-
-  const [name2, setName2] = useState()
-
   return (
-    <div className="App">
-      <HelloWorld />
-      <SayMyName name='João'/>
-      <SayMyName name={name}/>
-      <Person name={name} age={10}/>
-      <List />
-      <Event />
-      <Form />
-      <Conditional />
-      <h1>Renderização do Listas</h1>
-      <AnotherList myItems={myItems}/>
-      <AnotherList myItems={[]}/>
-      <h1>State Lift</h1>
-      <YourName setName2={setName2}/>
-      <Salutation name2={name2}/>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/company" element={<Company />}/>
+        <Route path="/contact" element={<Contact />}/>
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/pages/Company.tsx
@@ -0,0 +1,10 @@
+function Company() {
+    return (
+        <div>
+            <h1>Company</h1>
+            <p>Conteúdo da página</p>
+        </div>
+    )
+}
+
+export default Company
